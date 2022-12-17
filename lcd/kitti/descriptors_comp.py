23be8b35dd37cbfff69f40e918276c8cf0623dbc
JosefTAv <josef.thomas@epfl.ch>
2022-12-17 20:11:12 +0100

    Another small fix to descriptors comp

--- lcd/kitti/descriptors_comp.py
+++ lcd/kitti/descriptors_comp.py
@@ -17,29 +17,3 @@ def create_pc_to_img_array(arr_point_descriptors, arr_patch_descriptors):
     dist, corresponding_indexes = index.search(arr_point_descriptors, k) #search for k nearest neighbours
     return corresponding_indexes # corresponding images(nb img neighbours) = arr of indexes inside arr_point_descriptors
 
-
-import numpy as np
-
-d = 64                           # dimension
-nb = 100000                      # database size
-nq = 10000                       # nb of queries
-np.random.seed(1234)             # make reproducible
-xb = np.random.random((nb, d)).astype('float32')
-xb[:, 0] += np.arange(nb) / 1000.
-xq = np.random.random((nq, d)).astype('float32')
-xq[:, 0] += np.arange(nq) / 1000.
-
-import faiss                   # make faiss available
-index = faiss.IndexFlatL2(d)   # build the index
-print(index.is_trained)
-index.add(xb)                  # add vectors to the index
-print(index.ntotal)
-
-k = 1                          # we want to see 4 nearest neighbors
-D, I = index.search(xb[:5], k) # sanity check
-print(I)
-print(D)
-D, I = index.search(xq, k)     # actual search
-print(I[:5])                   # neighbors of the 5 first queries
-xb_found = xb[I][0,:,:]
-print(I[-5:])                  # neighbors of the 5 last queries
